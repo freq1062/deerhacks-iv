b66fa01e34052b794d109f7ff50dfcbee52ce673
Rasyid Rafi Pamuji <thenameis.rasyid@gmail.com>
2025-02-16 01:07:49 +0700

    Early Tasks Implementation

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,14 +1,14 @@
+// deerhacks-iv/client/src/pages/Home.jsx
 import React from "react";
-// import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import Map from "../components/Map.jsx";
-// import "leaflet/dist/leaflet.css";
-// import "@maptiler/sdk/dist/maptiler-sdk.css";
-// import "./map.css";
+import TaskToggleButtonTest from "../components/TaskToggleButtonTest"; // Import the test component
 
-export default function Home() {
+function Home() {
   return (
-    <main>
-      <Map />
-    </main>
+    <div style={{ height: "100vh", width: "100%" }}>
+      <h1>Task Toggle Button Test Page</h1>
+      <TaskToggleButtonTest /> {/* Render the TaskToggleButtonTest component */}
+    </div>
   );
 }
+
+export default Home;
