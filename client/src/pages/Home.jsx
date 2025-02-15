18767d0af58238349196dc37aa935620185bf7e6
Rasyid Rafi Pamuji <thenameis.rasyid@gmail.com>
2025-02-16 03:18:43 +0700

    added RegisterForm and LoginForm

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,12 +3,20 @@ import React, { useState } from "react";
 // import TaskToggleButtonTest from "../components/TaskToggleButtonTest"; // Import the test component
 import NavBar from "../components/NavBar.jsx";
 import Map from "../components/Map.jsx";
+import RegisterForm from "../components/RegisterForm.jsx"; // Import RegisterForm
+import LoginForm from "../components/LoginForm.jsx";   // Import LoginForm
 
 export default function Home() {
   return (
     <main>
       <Map />
       <NavBar className="navbar-overlay" />
+
+      {/* Add Login and Register Forms below NavBar and Map */}
+      <div style={{ display: 'flex', justifyContent: 'space-around', marginTop: '30px' }}>
+        <RegisterForm />  {/* Render Register Form */}
+        <LoginForm />     {/* Render Login Form */}
+      </div>
     </main>
   );
 }
