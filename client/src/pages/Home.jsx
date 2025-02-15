7aec7a26fa438e1b49b2ffed076321a9291816e2
Alex Min <alexminth@gmail.com>
2025-02-15 10:04:41 -0500

    map component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,27 +1,15 @@
 import React from "react";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
+// import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import Map from "../components/Map.jsx";
+// import "leaflet/dist/leaflet.css";
+// import "@maptiler/sdk/dist/maptiler-sdk.css";
+// import "./map.css";
 
 export default function Home() {
   return (
     <div style={{ height: "100vh", width: "100%" }}>
       <h1>Test</h1>
-      <MapContainer
-        center={[51.505, -0.09]}
-        zoom={13}
-        scrollWheelZoom={false}
-        className="leaflet-container"
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={[51.505, -0.09]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-      </MapContainer>
+      <Map />
     </div>
   );
 }
