6994775a5e3f22af36f1ede126450d4c0dd87930
daniel-halpern <dhalpern4040@gmail.com>
2025-02-15 11:30:27 -0500

    Added the path and roadway colored overlay

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -11,13 +11,31 @@ export default function Map() {
   maptilersdk.config.apiKey = "1Me6Sm6EB6jsGBjnCEL1";
 
   useEffect(() => {
-    if (map.current) return; // stops map from intializing more than once
+    if (map.current) return; // Prevent re-initialization
 
     map.current = new maptilersdk.Map({
       container: mapContainer.current,
       style: maptilersdk.MapStyle.STREETS,
       center: [tokyo.lng, tokyo.lat],
       zoom: zoom,
+    });
+
+    map.current.on("load", () => {
+      map.current.getStyle().layers.forEach((layer) => {
+        // Log all of the layer ids so that we can adjust which get displayed
+        // console.log("Layer ids:", layer.id);
+        // Only adjust layers that are drawn as lines
+        if (layer.id && layer.type === "line") {
+          // For larger roads, check for "road" in the ID and set red
+          if (layer.id.includes("road") && !layer.id.includes("Path")) {
+            map.current.setPaintProperty(layer.id, "line-color", "#FF0000");
+          }
+          // For smaller paths, check for "path" in the ID and set blue
+          if (layer.id.includes("Path")) {
+            map.current.setPaintProperty(layer.id, "line-color", "#0000FF");
+          }
+        }
+      });
     });
   }, [tokyo.lng, tokyo.lat, zoom]);
 
